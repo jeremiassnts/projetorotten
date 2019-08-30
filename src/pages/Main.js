7bda7bf126a384ad8f627e27f53389d37c230da1
Jeremias Santos <jeremiassnts3@gmail.com>
2019-08-30 09:29:34 -0300

    feat: send credentials in routes

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { InsideRoutes } from '../routes';
 import carrot from '../assets/carrot.svg'
 import postgresql from '../assets/postgresql.svg'
@@ -9,11 +9,15 @@ import '../styles/Main.css'
 
 export default function Main(props) {
     const { history } = props;
-    const [credentials] = useState(props.location.state)
+    const { state: credentials } = props.location;
+    const [activeButton, setActiveButton] = useState('')
     async function handleClick(e) {
         const title = e.target.title;
         history.push(`/main/${title}`, credentials)
     }
+    useEffect(() => {
+        setActiveButton(history.location.pathname)
+    },[history.location.pathname])
     return (
         <div className="main-container">
             <div className="menu-container">
@@ -23,25 +27,25 @@ export default function Main(props) {
                 </div>
                 <ul>
                     <li>
-                        <button title="consultas" onClick={handleClick}>
+                        <button title="consultas" onClick={handleClick} className={activeButton.endsWith("consultas") ? "on" : "off"}>
                             <img src={postgresql} alt="PostgreSQL" />
                             Consultas
                             </button>
                     </li>
                     <li>
-                        <button title="producoes" onClick={handleClick}>
+                        <button title="producoes" onClick={handleClick} className={activeButton.endsWith("producoes") ? "on" : "off"}>
                             <img src={movie} alt="Produção" />
                             Produções
                         </button>
                     </li>
                     <li>
-                        <button title="artistas" onClick={handleClick}>
+                        <button title="artistas" onClick={handleClick} className={activeButton.endsWith("artistas") ? "on" : "off"}>
                             <img src={person} alt="Artista" />
                             Artistas
                         </button>
                     </li>
                     <li>
-                        <button title="estudios" onClick={handleClick}>
+                        <button title="estudios" onClick={handleClick} className={activeButton.endsWith("estudios") ? "on" : "off"}>
                             <img src={industry} alt="Estúdio" />
                             Estúdios
                         </button>
